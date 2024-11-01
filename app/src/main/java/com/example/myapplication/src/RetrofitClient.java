8b2ab7aa6f28c10d922afef976dd93b081b81152
PersieDaCoder <thinhdpham2510@gmail.com>
2024-11-01 17:40:14 +0700

    build: reconfigure retrofit and add result entity for base results

--- app/src/main/java/com/example/myapplication/src/RetrofitClient.java
+++ app/src/main/java/com/example/myapplication/src/RetrofitClient.java
@@ -1,5 +1,11 @@
 package com.example.myapplication.src;
 
+import android.content.Context;
+
+import com.example.myapplication.utils.sharedpreferences.AuthInterceptor;
+import com.example.myapplication.utils.sharedpreferences.TokenManager;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,10 +13,16 @@ public class RetrofitClient {
     private static Retrofit retrofit;
     private static final String BASE_URL = "https://bookstore.persiehomeserver.com";
 
-    public static Retrofit getRetrofitInstance() {
+    public static Retrofit getRetrofitInstance(Context context) {
         if (retrofit == null) {
+            TokenManager tokenManager = new TokenManager(context);
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(new AuthInterceptor(tokenManager))
+                    .build();
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
